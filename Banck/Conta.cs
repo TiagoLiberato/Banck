4db012c9cf94fb5ad8971252f5c838f04f2175bb
Tiago Liberato <tiagolfsm@gmail.com>
2022-02-03 16:22:14 -0300

    Adicionar arquivos de projeto.

---
+++ Banck/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banck
+{
+    public class Conta
+    {
+        public double Saldo { get; set; }   
+        public double Limite { get; set; }  
+        public int Numero { get; set; } 
+
+    }
+}
---
+++ Banck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Banck;
+
+namespace Banck
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("============Projeto Bancário===============");
+            Conta conta1 = new Conta();
+            conta1.Saldo = 1500;
+            conta1.Limite = 500;
+            conta1.Numero = 123;
+
+            Conta conta2= new Conta();
+            conta2.Saldo = 2000;
+            conta2.Limite = 600;
+            conta2.Numero = 456;
+            
+            Console.WriteLine("Ola Cliente seu saldo em Conta é :" + conta1.Saldo);
+            Console.WriteLine("limite de saque é :" + conta1.Limite);
+            Console.WriteLine("O Numero da Conta é :" + conta1.Numero);
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Console.WriteLine("Ola Cliente seu saldo em Conta é :" + conta2.Saldo);
+            Console.WriteLine("limite de saque é :" + conta2.Limite);
+            Console.WriteLine("O Numero da Conta é :" + conta2.Numero);
+            
+        }
+    }
+}
